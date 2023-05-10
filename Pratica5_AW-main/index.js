1691b29267acbda794692dd5344c114104855ea7
Thaís Carvalho <111368656+thaiscarv@users.noreply.github.com>
2023-05-10 20:44:59 -0300

    Add files via upload

---
+++ Pratica5_AW-main/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require("axios");
+var cors = require("cors");
+const CLIENT_ID = "<YOUR GITHUB CLIENT ID>";
+const CLIENT_SECRET = "<YOUR GITHUB CLIENT SECRET>";
+const GITHUB_URL = "https://github.com/login/oauth/access_token";
+const app = express();
+app.use(cors({ credentials: true, origin: true }));
+app.get("/oauth/redirect", (req, res) => {
+axios({
+method: "POST",
+url: `${GITHUB_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`,
+headers: {
+Accept: "application/json",
+},
+}).then((response) => {
+res.redirect(
+`http://localhost:3000?access_token=${response.data.access_token}`
+);
+});
+});
+const PORT = 8080;
+app.listen(PORT, () => {
+console.log(`Listening at port ${PORT}`);
+});
